1fc2735762a3167804f690155ac58facdcc2b56a
Douglas <douglas_timaofiel@hotmail.com>
2023-06-14 22:19:10 -0300

    Implemented the doctor registration functionality and created the Medico entity with JPA mapping

---
+++ src/main/java/medi/pro/api/endereco/Endereco.java
@@ -0,0 +1,24 @@
+package medi.pro.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+		
+	private String logradouro;
+	private String bairro;
+	private String cep;
+	private String numero;
+	private String complemento;
+	private String cidade;
+	private String uf;
+	
+}
