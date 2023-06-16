af6ac26737a96e2b4c6a62369ea5e8e7dc71e067
Douglas <douglas_timaofiel@hotmail.com>
2023-06-16 11:30:20 -0300

    Added Flyway migration for creating the 'medicos' table

--- src/main/java/medi/pro/api/controller/MedicoController.java
+++ src/main/java/medi/pro/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import medi.pro.api.medico.DadosCadastroMedico;
 import medi.pro.api.medico.Medico;
 import medi.pro.api.medico.MedicoRepository;
@@ -18,6 +19,7 @@ public class MedicoController {
 	private MedicoRepository repository;
 	
 	@PostMapping
+	@Transactional
 	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
 	}
