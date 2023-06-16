116335b8c1e605f5c4d5029265809eb3923f3a27
Douglas <douglas_timaofiel@hotmail.com>
2023-06-16 12:32:40 -0300

    Implemented input validation using Bean Validation

--- src/main/java/medi/pro/api/endereco/DadosEndereço.java
+++ src/main/java/medi/pro/api/endereco/DadosEndereço.java
@@ -1,5 +1,28 @@
 package medi.pro.api.endereco;
 
-public record DadosEndereço(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereço(
+		
+		@NotBlank
+		String logradouro, 
+		
+		@NotBlank
+		String bairro, 
+		
+		@NotBlank
+		@Pattern(regexp = "\\d{8}")
+		String cep, 
+		
+		@NotBlank
+		String cidade, 
+		
+		@NotBlank
+		String uf, 
+		
+		String complemento, 
+		
+		String numero) {
 
 }
--- src/main/java/medi/pro/api/medico/DadosCadastroMedico.java
+++ src/main/java/medi/pro/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,30 @@
 package medi.pro.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import medi.pro.api.endereco.DadosEndereço;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereço endereco) {
+public record DadosCadastroMedico(
+		
+		@NotBlank
+		String nome, 
+		
+		@NotBlank
+		@Email
+		String email, 
+		
+		@NotBlank
+		@Pattern(regexp = "\\d{4,6}")
+		String crm, 
+		
+		@NotNull
+		Especialidade especialidade, 
+		
+		@NotNull
+		@Valid
+		DadosEndereço endereco) {
 
 }
