71a57932a5d7332b392554353a65001389423a20
Douglas <douglas_timaofiel@hotmail.com>
2023-06-17 17:57:11 -0300

    Implemented pagination for doctor listing

--- src/main/java/medi/pro/api/controller/MedicoController.java
+++ src/main/java/medi/pro/api/controller/MedicoController.java
@@ -1,8 +1,10 @@
 package medi.pro.api.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,8 @@ public class MedicoController {
 		repository.save(new Medico(dados));
 	}
 	
-	public List<DadosListagemMedico> listar(){
-		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	@GetMapping
+	public Page<DadosListagemMedico> listar(Pageable paginacao){
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 }
