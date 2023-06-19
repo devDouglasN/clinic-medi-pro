bed0ff255ecfe097d34123f3f79ba654015837d4
Douglas <douglas_timaofiel@hotmail.com>
2023-06-19 10:15:41 -0300

    Implementation of traditional deletion for doctors

--- src/main/java/medi/pro/api/controller/MedicoController.java
+++ src/main/java/medi/pro/api/controller/MedicoController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,4 +47,9 @@ public class MedicoController {
 		medico.atualizarInformacoes(dados);
 	}
 	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
