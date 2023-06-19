e5781923591ebd0965854a56a278a68fa248d460
Douglas <douglas_timaofiel@hotmail.com>
2023-06-19 10:59:23 -0300

    Implementation of logical deletion for doctors

--- src/main/java/medi/pro/api/medico/Medico.java
+++ src/main/java/medi/pro/api/medico/Medico.java
@@ -31,12 +31,17 @@ public class Medico {
     private String telefone;
 
     @Enumerated(EnumType.STRING)
+    
+   
     private Especialidade especialidade;
 
     @Embedded
     private Endereco endereco;
+    
+    private Boolean ativo;
    
 	public Medico(DadosCadastroMedico dados) {
+		this.ativo = true;
     	this.nome = dados.nome();
     	this.email = dados.email();
     	this.telefone = dados.telefone();
@@ -55,5 +60,9 @@ public class Medico {
 		if(dados.endereco() != null) {
 			this.endereco.atualizarInformacoes(dados.endereco());
 		}
+	}
+
+	public void excluir() {
+		this.ativo = false;
 	}
 }
