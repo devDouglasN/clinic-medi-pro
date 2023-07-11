773500cf8bd492c4e8501f1efa833d054af4fffd
Douglas <douglas_timaofiel@hotmail.com>
2023-07-11 15:28:35 -0300

    Refactoring HTTP status codes and implementation of doctor details endpoint

--- src/main/java/medi/pro/api/controller/MedicoController.java
+++ src/main/java/medi/pro/api/controller/MedicoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medi.pro.api.medico.DadosAtualizacaoMedico;
 import medi.pro.api.medico.DadosCadastroMedico;
+import medi.pro.api.medico.DadosDetalhamentoMedico;
 import medi.pro.api.medico.DadosListagemMedico;
 import medi.pro.api.medico.Medico;
 import medi.pro.api.medico.MedicoRepository;
@@ -31,26 +33,31 @@ public class MedicoController {
 	
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+	public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+		var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(page);
 	}
 	
 	@PutMapping
 	@Transactional
-	public void atualizar (@RequestBody @Valid DadosAtualizacaoMedico dados) {
+	public ResponseEntity atualizar (@RequestBody @Valid DadosAtualizacaoMedico dados) {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 	}
 	
 	@DeleteMapping("/{id}")
 	@Transactional
-	public void excluir(@PathVariable Long id) {
+	public ResponseEntity excluir(@PathVariable Long id) {
 		var medico = repository.getReferenceById(id);
 		medico.excluir();
+		
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/medi/pro/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,10 @@
+package medi.pro.api.medico;
+
+import medi.pro.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+	
+	public DadosDetalhamentoMedico(Medico medico) {
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+	}
+}
