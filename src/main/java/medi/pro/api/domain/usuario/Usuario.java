eee4acbe46f68049b8d44870705e81d0232aac48
Douglas <douglas_timaofiel@hotmail.com>
2023-07-16 11:56:10 -0300

    Add UserEntity and User Migration

---
+++ src/main/java/medi/pro/api/domain/usuario/Usuario.java
@@ -0,0 +1,27 @@
+package medi.pro.api.domain.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Table(name = "usuarios")
+@Entity(name = "usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String senha;
+}
