3d6426c62e17b3456ad3a88fa96ea88438cc72d2
Douglas <douglas_timaofiel@hotmail.com>
2023-07-19 15:26:12 -0300

    Implemented authentication controller and configure authentication manager

--- src/main/java/medi/pro/api/domain/usuario/AutenticacaoService.java
+++ src/main/java/medi/pro/api/domain/usuario/AutenticacaoService.java
@@ -1,5 +1,6 @@
 package medi.pro.api.domain.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class AutenticacaoService implements UserDetailsService{
 	
+	@Autowired
 	private UsuarioRepository repository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return repository.findByLogin(username);
 	}
-	
-	
+		
 }
---
+++ src/main/java/medi/pro/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,5 @@
+package medi.pro.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+
+}
