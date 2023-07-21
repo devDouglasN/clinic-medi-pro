60f77d6b013165ada58cf5928040b692a189ad70
Douglas <douglas_timaofiel@hotmail.com>
2023-07-21 10:25:24 -0300

    Implemented security filter for token validation

---
+++ src/main/java/medi/pro/api/infra/security/SecurityFilter.java
@@ -0,0 +1,21 @@
+package medi.pro.api.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+		filterChain.doFilter(request, response);
+	}
+
+}
