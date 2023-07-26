458ec55c8c088c4aaf9b1429ecc3061cfabad7c5
Douglas <douglas_timaofiel@hotmail.com>
2023-07-26 17:30:54 -0300

    Implementing Authorization and Authentication with Tokens

--- src/main/java/medi/pro/api/infra/security/SecurityFilter.java
+++ src/main/java/medi/pro/api/infra/security/SecurityFilter.java
@@ -3,6 +3,8 @@ package medi.pro.api.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,19 +12,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import medi.pro.api.domain.usuario.UsuarioRepository;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository repository;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-	        var tokenJWT = recuperarToken(request);
+			var tokenJWT = recuperarToken(request);
 
 	        if(tokenJWT != null) {
 	        	var subject = tokenService.getSubject(tokenJWT);
+	        	var usuario = repository.findByLogin(subject);
+	        	var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+	        	
+	        	SecurityContextHolder.getContext().setAuthentication(authentication);
 	        }
 	        
 	        filterChain.doFilter(request, response);
