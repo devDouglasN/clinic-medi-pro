59c3fd78a85fa8ee668e78535d09bc55acca936a
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-12 12:17:55 -0300

    Added JPA annotations for Paciente

--- src/main/java/medi/pro/api/domain/paciente/Paciente.java
+++ src/main/java/medi/pro/api/domain/paciente/Paciente.java
@@ -2,16 +2,28 @@ package medi.pro.api.domain.paciente;
 
 import java.util.Date;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import medi.pro.api.domain.Endereco;
 
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -20,5 +32,8 @@ public class Paciente {
 	private String genero;
 	private String registroSaude;
 	
+	@Embedded
 	private Endereco endereco;
+	
+	private Boolean ativo;
 }
