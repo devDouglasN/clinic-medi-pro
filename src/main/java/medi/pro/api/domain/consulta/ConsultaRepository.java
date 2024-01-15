66a7bd334ecc461c44b26043e0786c6c8188494d
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-15 11:48:24 -0300

    Repository for Consulta

---
+++ src/main/java/medi/pro/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,6 @@
+package medi.pro.api.domain.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
+}
