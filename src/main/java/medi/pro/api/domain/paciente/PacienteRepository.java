e47c09f0411558c6fffc2a7871a94077fbb1de68
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-02-02 13:47:30 -0300

    Query to check active doctor

--- src/main/java/medi/pro/api/domain/paciente/PacienteRepository.java
+++ src/main/java/medi/pro/api/domain/paciente/PacienteRepository.java
@@ -3,10 +3,18 @@ package medi.pro.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
 	
 	Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 
-	boolean findAtivoById(Long idPaciente);
-}
+	@Query("""
+			select p.ativo
+			from Paciente p
+			where
+			p.id = :id
+			""")
+	boolean findAtivoById(Long id);
+	}
+
